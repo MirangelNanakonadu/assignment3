4826158260ef032458995f172565bf64501fd438
unknown <akosuamaxine@gmail.com>
2018-09-02 09:54:21 -0700

    assignment3

---
+++ assignment.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int number;
+	cout<<"Enter your marks to determine your grade"<<endl;
+	cin>>number;
+	
+	if(number>=80 && number <= 100)
+	{
+		cout<<"Grade A"<<endl;
+	}else if(number>=75 && number<= 79)
+	{
+		cout<<"B+"<<endl;
+	}else if(number>=70 && number<=74)
+	{
+			cout<<"B"<<endl;
+		}else if(number>=65 && number<=69)
+		{
+			cout<<"C+";
+		}else if(number>=60 && number<=64)
+		{
+			cout<<"C";
+		}else if(number>=55 && number<=59)
+		{
+			cout<<"D+";
+		}else if(number>=50 && number<=54)
+		{
+			cout<<"D";
+		}else if(number>=45 && number<=49)
+		{
+			cout<<"E";
+		}else{
+			cout<<"FAILED";
+		}
+		
+		return 0;
+	}
+
+
+
